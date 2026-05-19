12947ac75d6c7dc62d368ad7d3d5d59011bef165
TGScotgov <wisbeech@gmail.com>
2026-05-19 12:11:21 +0000

    adding infra

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.43.0"
+    }
+  }
+
+  required_version = "1.15.3"
+
+  cloud {
+
+    organization = "ACG-Wisbeech-Terraform-Demos"
+
+    workspaces {
+      name = "Hands-on_with_terraform_on_Azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "813-7cdbf4b0-hands-on-with-terraform-on-azure"
+  location = "West US"
+}
+
+module "securestorage" {
+  source               = "app.terraform.io/ACG-Wisbeech-Terraform-Demos/securestorage/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "wisbeech1226891178"
+}
